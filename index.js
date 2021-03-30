c9eea57540a3170ffab40ac6735f0f3f34184c5c
moises <mhdezcoronado@gmail.com>
2021-03-30 22:46:23 +0200

    Add buuner menu & responsiveness to mobile devices

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function displayNone() {
+    let heder = document.getElementById('header')
+    heder.style.display = 'none'
+
+    let main = document.getElementById('main')
+    main.style.display = 'none'
+
+    let footer = document.getElementById('footer')
+    footer.style.display = 'none'
+
+    let humberger = document.getElementById('humberger')
+    humberger.style.display = 'block'
+}
+
+function displayBack() {
+    let heder = document.getElementById('header')
+    heder.style.display = 'block'
+
+    let main = document.getElementById('main')
+    main.style.display = 'block'
+
+    let footer = document.getElementById('footer')
+    footer.style.display = 'block'
+
+    let humberger = document.getElementById('humberger')
+    humberger.style.display = 'none'
+}
+
